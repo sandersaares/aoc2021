331489f6de752deeec35f1ae4a01e354caa3efd8
Sander Saares <sasaares@microsoft.com>
2021-12-05 16:38:42 +0200

    Add fuzzing script for easy execution

---
+++ Aoc2021.Fuzz/Fuzz.ps1
@@ -0,0 +1,26 @@
+﻿$ErrorActionPreference = "Stop"
+
+if (!$IsLinux) {
+    Write-Error "You must run this script on Linux. WSL works fine."
+}
+
+# Prerequisite: execute https://raw.githubusercontent.com/Metalnem/sharpfuzz/master/build/Install.sh
+
+$libraryPath = Join-Path $PSScriptRoot "Aoc2021.dll"
+$entrypointPath = Join-Path $PSScriptRoot "Aoc2021.Fuzz.dll"
+$inputPath = Join-Path $PSScriptRoot "D2P2"
+$outputPath = Join-Path $PSScriptRoot "out"
+
+function VerifySuccess() {
+    if ($LASTEXITCODE -ne 0) {
+        Write-Error "Command failed with exit code $LASTEXITCODE"
+    }
+}
+
+# Instrument the library.
+& sharpfuzz $libraryPath
+VerifySuccess
+
+# Start!
+# We need -m to increase the default (tiny) memory limit.
+& afl-fuzz -i $inputPath -o $outputPath -t 10000 -m 10000 dotnet $entrypointPath
